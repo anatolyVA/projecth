450395fa7c5e2cc513b6c8936f3ebcd92cdd628e
anatolyVA <nsukhankov@inbox.ru>
2023-11-10 21:17:59 +0300

    upd3

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,21 +1,42 @@
-import React from 'react'
-
+import React from "react";
+import './style.css'
+import Link from 'next/link'
+import { StaticImageData } from "next/image";
 type Props = {
-    image: string
-}
+  image: string;
+  type: {
+    type1: string
+  }
+  name: string
+};
 
-function Card({image} : Props) {
+function Card({ image, type, name }: Props) {
   return (
-    <div className='card'>
-        <div style={{backgroundImage: `url(${image})`}}>
-
+    <div className="card">
+      <div
+        style={{
+          backgroundImage: `rgba(0,0,0, 0.5), url(${image})`,
+          backgroundPosition: "center",
+          backgroundSize: "100%",
+          backgroundRepeat: "no-repeat",
+          backgroundColor: 'gray'
+        }}
+        className="card-image"
+      >
+      </div>
+      <div className="body">
+        <div className='header'>
+            <div className="types">
+                <span>{type.type1}</span>
+            </div>
+            <h2>{name}</h2>
         </div>
-        <div className='body'>
-
-        </div>
-
+        <hr style={{opacity: '0.2'}}></hr>
+        <p>Описание всех описаний</p>
+        <Link href='/' className="button button_filled button_medium">Подробнее</Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
