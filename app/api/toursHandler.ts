27c7cf192aec105dcebab1951a8b7423ed32b66b
po1emic <den.riabchenkov@gmail.com>
2023-11-11 01:35:20 +0300

    api update

---
+++ app/api/toursHandler.ts
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'GET') {
+    const body = req.body
+    console.log(body)
+
+    res.status(200).end()
+  }
+}
+
+export default handler
