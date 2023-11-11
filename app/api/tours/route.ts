8529e35c6c6bba99e20b34bd288d11a80036219c
po1emic <den.riabchenkov@gmail.com>
2023-11-11 07:37:53 +0300

    db models expansion

--- app/api/tours/route.ts
+++ app/api/tours/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 const prisma = new PrismaClient()
 
 export async function GET(req: Request) {
-    const tours = await prisma.typesOfAttraction.findMany()
+    const tours = await prisma.tour.findMany()
 
     return NextResponse.json(tours)
 }
