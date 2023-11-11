176dd2f567969c5507308e300c04b06a68f9dc08
anatolyVA <nsukhankov@inbox.ru>
2023-11-11 03:02:42 +0300

    ura

--- app/attraction/[id]/page.tsx
+++ app/attraction/[id]/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import { AiOutlineUser } from "react-icons/ai";
 import "./style.css";
 type Props = {
   params: {
@@ -10,11 +11,28 @@ type Props = {
 function Attraction({ params: { id } }: Props) {
   return (
     <main className="attraction-page">
-      <div className="attraction-page_hero">
-        <div className="attraction-page_hero-center">
-          <span>Исторические достопримечательности</span>
-          <h1>Громовая башня</h1>
-          <p>Одна из башен Смоленскогоо кремля</p>
+      <div className="attraction-page_background">
+        <div className="attraction-page_background-center">
+          <div className="attraction-header">
+            <div className="header-items">
+              <span style={{ opacity: "0.5" }}>
+                Исторические достопримечательности
+              </span>
+              <h1>Громовая башня</h1>
+              <p style={{ opacity: "0.5", fontWeight: "700" }}>
+                Одна из башен Смоленскогоо кремля
+              </p>
+            </div>
+            <div className="header-buttons">
+              <button className="iconbutton iconbutton_outlined">
+                <AiOutlineUser size="1.5em"></AiOutlineUser>
+              </button>
+              <button className="button button_outlined button_medium">
+                Показать на карте
+              </button>
+            </div>
+          </div>
+          <hr style={{ width: "100%", opacity: 0.2 }}></hr>
         </div>
       </div>
     </main>
